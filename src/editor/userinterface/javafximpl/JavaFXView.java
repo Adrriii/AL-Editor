913c4a1c80bdcbba2729a35c382155c960cc4ce6
Adri- <adrien.boitelle@gmail.com>
2020-04-12 16:11:46 +0200

    add back for toolbar and menu

--- src/editor/userinterface/javafximpl/JavaFXView.java
+++ src/editor/userinterface/javafximpl/JavaFXView.java
@@ -2,6 +2,8 @@ package editor.userinterface.javafximpl;
 
 import editor.application.App;
 import editor.domain.Canvas;
+import editor.domain.Toolbar;
+import editor.domain.menu.TopMenu;
 import editor.userinterface.View;
 import javafx.scene.paint.Color;
 
@@ -26,30 +28,45 @@ public class JavaFXView implements View {
 
             this.started = true;
         }
-        drawCanvas();
-        drawToolBar();
-        drawMenu();
+        drawCanvas(App.model.getCanvas());
+        drawToolBar(App.model.getToolbar());
+        drawMenu(App.model.getTopMenu());
     }
 
-    public void drawCanvas() {
-        Canvas canvas = App.model.getCanvas();
-
+    public void drawCanvas(Canvas canvas) {
         javafx.scene.shape.Rectangle canvasRectangle = new javafx.scene.shape.Rectangle();
         
         canvasRectangle.setX(canvas.pos_x);
         canvasRectangle.setY(canvas.pos_y);
         canvasRectangle.setWidth(canvas.width);
         canvasRectangle.setHeight(canvas.height);
-        canvasRectangle.setFill(Color.BLACK);
+        canvasRectangle.setFill(Color.WHITE);
         
         JavaFXApplication.addToRoot(canvasRectangle);
     }
 
-    public void drawToolBar() {
-
+    public void drawToolBar(Toolbar toolbar) {
+        javafx.scene.shape.Rectangle rectangle = new javafx.scene.shape.Rectangle();
+        
+        rectangle.setX(toolbar.pos_x);
+        rectangle.setY(toolbar.pos_y);
+        rectangle.setWidth(toolbar.width);
+        rectangle.setHeight(toolbar.height);
+        rectangle.setFill(Color.GREY);
+        
+        JavaFXApplication.addToRoot(rectangle);
     }
 
-    public void drawMenu() {
+    public void drawMenu(TopMenu menu) {
+        javafx.scene.shape.Rectangle rectangle = new javafx.scene.shape.Rectangle();
+        
+        rectangle.setX(menu.pos_x);
+        rectangle.setY(menu.pos_y);
+        rectangle.setWidth(menu.width);
+        rectangle.setHeight(menu.height);
+        rectangle.setFill(Color.LIGHTGREY);
+        
+        JavaFXApplication.addToRoot(rectangle);
 
     }
 
