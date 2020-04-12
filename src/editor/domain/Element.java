0c5c2e78276aa7810ed21af6b2e9085d7d1e753c
Adri- <adrien.boitelle@gmail.com>
2020-04-12 17:26:50 +0200

    add left/top boundaries

--- src/editor/domain/Element.java
+++ src/editor/domain/Element.java
@@ -45,8 +45,16 @@ public class Element extends Drawable implements ISerializable, IControllable, I
     }
 
     public void Update(int new_pos_x, int new_pos_y) {
-        pos_x = new_pos_x;
-        pos_y = new_pos_y;
+        if(new_pos_x > 0) {
+            pos_x = new_pos_x;
+        } else {
+            pos_x = 0;
+        }
+        if(new_pos_y > 0) {
+            pos_y = new_pos_y;
+        } else {
+            pos_y = 0;
+        }
 
         Update();
     }
