193e1fc4c6d31ebf6c837b8e2d3ef444f593d806
Adri- <adrien.boitelle@gmail.com>
2020-05-17 16:29:16 +0200

    add tests base

---
+++ src/tests/ModelTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import editor.model.Model;
+
+public class ModelTests {
+    
+    
+    @Test
+    public void modelCreateTest() {
+        Model model = new Model();
+
+        assertTrue(true);
+    }
+}
